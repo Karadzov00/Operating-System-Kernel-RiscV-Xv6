fe4fa3ac4a5ef511705b6f1f4a108f3c04771e10
Karadzov00 <caka.azdaha@gmail.com>
2022-06-19 17:44:26 +0200

    updated overloaded operators

--- src/_new.cpp
+++ src/_new.cpp
@@ -6,18 +6,24 @@
 #include "../h/MemoryAllocator.hpp"
 using size_t = decltype(sizeof(0));
 
+
 void* operator new(size_t n){
-    return MemoryAllocator::mem_alloc(n);
+
+    MemoryAllocator* memAlc = &MemoryAllocator::getInstance();
+    return memAlc->mem_alloc(n);
 }
 
 void* operator new[](size_t n){
-    return MemoryAllocator::mem_alloc(n);
+    MemoryAllocator* memAlc = &MemoryAllocator::getInstance();
+    return memAlc->mem_alloc(n);
 }
 
 void operator delete(void* p) noexcept{
-    MemoryAllocator::mem_free(p);
+    MemoryAllocator* memAlc = &MemoryAllocator::getInstance();
+    memAlc->mem_free(p);
 }
 
 void operator delete[] (void* p)noexcept{
-    MemoryAllocator::mem_free(p);
+    MemoryAllocator* memAlc = &MemoryAllocator::getInstance();
+    memAlc->mem_free(p);
 }
