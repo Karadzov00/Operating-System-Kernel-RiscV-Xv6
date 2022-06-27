da30c94c4c90063e1d3a506daf08adb608f4cc01
Karadzov00 <caka.azdaha@gmail.com>
2022-06-27 19:59:06 +0200

    semaphores work!!

--- src/KSemaphore.cpp
+++ src/KSemaphore.cpp
@@ -7,20 +7,24 @@
 List<_thread> KSemaphore::blocked;
 
 int KSemaphore::wait() {
-    if(--val < 0){
-        _thread *old = _thread::running;
-        if (!old->isFinished()) { blocked.addLast(old); }
-        _thread::running = Scheduler::get();
+    if(--(this->val) < 0){
 
+        _thread* old = _thread::running;
+        if(!old->isFinished()) { KSemaphore::blocked.addLast(old); }
+        _thread::running = Scheduler::get();
         _thread::contextSwitch(&old->context, &_thread::running->context);
+
     }
     return 0;
 }
 
 void KSemaphore::signal() {
-    if (++val <= 0) {
-        _thread *p = blocked.removeFirst();
-        Scheduler::put(p);
+    if(++(this->val) <=0){
+        if(blocked.peekFirst()!= 0){
+            _thread* thread = KSemaphore::blocked.removeFirst();
+//            _thread* thread = blocked.peekFirst();
+            Scheduler::put(thread);
+        }
     }
 }
 
