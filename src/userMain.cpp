c21de8f3114be74cd52c735227bdcdaf2c6b537b
Karadzov00 <caka.azdaha@gmail.com>
2022-06-30 01:34:08 +0200

    MemoryAllocator works!!!

--- src/userMain.cpp
+++ src/userMain.cpp
@@ -11,7 +11,7 @@
 void userMain() {
 //    Threads_Sems_C_API_test(); // Davidov test
 //    Threads_C_API_test(); // zadatak 2., niti C API i sinhrona promena konteksta
-//    Threads_CPP_API_test(); // zadatak 2., niti CPP API i sinhrona promena konteksta
+    Threads_CPP_API_test(); // zadatak 2., niti CPP API i sinhrona promena konteksta
 
 //    producerConsumer_C_API(); // zadatak 3., kompletan C API sa semaforima, sinhrona promena konteksta
 //    producerConsumer_CPP_Sync_API(); // zadatak 3., kompletan CPP API sa semaforima, sinhrona promena konteksta
